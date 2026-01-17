366a9961c0c693663ce42cf7a7400865d686fbe3
Umer Akhlaq <umerikhlaq160@gmail.com>
2026-01-17 18:07:36 +0500

    Fix case sensitivity and paths

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import HeroSection from "../components/HeroSection";
 import MainstayMenu from "../components/MainstayMenu";
 import FeaturedDish from "../components/FeatureCards";
 import ProductGrid from "../components/ProductGrid";
-import TopPicks from "../components/topPick";
+import TopPicks from "../components/TopPicks";
 import ReservationSection from "../components/Booking";
 import Footer from "../components/Footer";
 
